71cbb7f390e4d53227595561b7e00936825c701c
Mekhrona <mehrona.nm@gmail.com>
2021-03-28 19:38:00 +0300

    directory change

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -20,3 +20,21 @@ sum:=types.Money(0)
 
 	return sum
 }
+func PaymentSources(cards []types.Card) []types.PaymentSource {
+	var paymentsSource []types.PaymentSource
+	for _, card := range cards {
+
+		if card.Active && card.Balance > 0 {
+			paymentSource := types.PaymentSource{
+				Type:    "card",
+				Number:  string(card.PAN),
+				Balance: card.Balance,
+			}
+
+			paymentsSource = append(paymentsSource, paymentSource)
+		}
+
+	}
+
+	return paymentsSource
+}
--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -5,6 +5,45 @@ import (
 	"fmt")
 
 
+
+
+	func ExamplePaymentSource() {
+
+		cards:= [] types.Card{
+			{
+				PAN :     "6768 6845 3456",
+				Balance:  58949,
+				Active:   true,
+			},
+			{
+				PAN :     "7848 6845 3456",
+				Balance:  -89,
+				Active:   true,
+			},
+			{
+				PAN :     "9856 6845 3456",
+				Balance:  899,
+				Active:   false,
+			},
+			{
+				PAN :     "6768 6845 3456",
+				Balance:  89512,
+				Active:   true,
+			},
+		}
+		
+			PaymentSources:=PaymentSources(cards)
+			for _, paymentSource := range PaymentSources {
+		
+				fmt.Println(paymentSource.Balance)
+			}
+			
+		
+			//Output: 58949
+			// 89512
+			
+		}
+
 func ExampleTotal() {
 
 	cards:=[] types.Card{
